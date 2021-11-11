f3724eba1d4113f7f40725310c5b966f2216b127
sebagadisk <sebexk3@gmail.com>
2021-11-11 10:07:15 +0300

    modified header's

--- index.php
+++ index.php
@@ -3,12 +3,12 @@ date_default_timezone_set('Africa/Nairobi');
 ?>
 <html>
 <head>
-    <title>Heroku deployment demo</title>
+    <title>server deployment demo</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 <body>
 <div class="container">
-    <h1>deployed on heroku</h1>
+    <h1>deployed on server</h1>
     <h3><?php echo date("Y-m-d")?></h3>
 </div>
 </body>
