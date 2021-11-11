ec8d424c7e6673a6f495a8650ab2a17d72f758e3
sebagadisk <sebexk3@gmail.com>
2021-11-11 09:52:55 +0300

    modified inex.php

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
 <?php
+date_default_timezone_set('Africa/Nairobi');
+?>
+<html>
+<head>
+    <title>Heroku deployment demo</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+</head>
+<body>
+<div class="container">
+    <h1>deployed on heroku</h1>
+    <h3><?php echo date("Y-m-d")?></h3>
+</div>
+</body>
+</html>
